e909ba8146685c365af77669986756c045513274
ogarridojimenez <ogarridojimenez@gmail.com>
2025-07-31 10:10:01 -0400

    feat: add separate files for route handlers

--- router/router.go
+++ router/router.go
@@ -3,11 +3,11 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialice() {
-	//inicializa una ruta utilizando la configuración por defecto de gin
+	//TODO: inicializa una ruta utilizando la configuración por defecto de gin
 	router := gin.Default()
 
-	//definiendo una ruta
+	//TODO: definiendo una ruta
 	inicializeRoutes(router)
-	// Run the server
-	router.Run(":8080") // listen and serve on 0.0.0.0:8080
+	//TODO: Run the server
+	router.Run(":8080") //TODO: listen and serve on 0.0.0.0:8080
 }
