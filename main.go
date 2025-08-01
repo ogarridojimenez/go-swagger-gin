d6ae903eccfab8927c5961763955ff9a4915e87e
ogarridojimenez <ogarridojimenez@gmail.com>
2025-07-31 21:47:11 -0400

    feat:add custom logger

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
-import "github.com/ogarridojimenez/go-swagger-gin/router"
+import (
+	"github.com/ogarridojimenez/go-swagger-gin/config"
+	"github.com/ogarridojimenez/go-swagger-gin/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
-	// inicializa router
+
+	logger = config.GetLogger("main")
+
+	//TODO: Initialize Configs
+
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+	//TODO: Initialize Router
 	router.Initialice()
 
 }
